cd0fe0aa498dd78893fcd1ab7cdffc5443aac04f
opevolution <alexandre_defendi@hotmail.com>
2014-03-26 22:35:06 -0300

    Versão 0.0.002

--- account_flow_cash/account_flow_cash.py
+++ account_flow_cash/account_flow_cash.py
@@ -112,15 +112,15 @@ class account_flow_cash(osv.osv_memory):
         #vlAcum = vlDeb - vlCred
         if journal_id:
             if account_cd == '1.01.01.02.003':
-                vlAcum = vlDeb - (vlCred + (-5116.06))
+                vlAcum = vlDeb - (vlCred + 5116.06)
             elif account_cd == '1.01.01.02.004':
-                vlAcum = vlDeb - (vlCred + 11431.33)
+                vlAcum = vlDeb - (vlCred + (-11431.33))
             elif account_cd == '1.01.01.02.005':
-                vlAcum = vlDeb - (vlCred + (-688,24))
+                vlAcum = vlDeb - (vlCred + 688,24)
             elif account_cd == '1.01.01.02.006':
-                vlAcum = vlDeb - (vlCred + (-805.95))
+                vlAcum = vlDeb - (vlCred + 805.95)
             elif account_cd == '1.01.01.02.007':
-                vlAcum = vlDeb - (vlCred + (-192.81))
+                vlAcum = vlDeb - (vlCred + 192.81)
         else:
             vlAcum = vlDeb - (vlCred + 4628.27)
         
