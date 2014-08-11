9ea654f0b70cce94eb5ceb29aa1dada6c927b63b
opevolution <alexandre_defendi@hotmail.com>
2014-08-11 20:23:39 -0300

    Filtro

--- account_flow_cash/res_company.py
+++ account_flow_cash/res_company.py
@@ -4,8 +4,8 @@ class res_company(osv.osv):
 
     _inherit = 'res.company'
     _columns = {
-                'account_expense_id': fields.many2one('account.account', u'Conta Despesa Padrao',domain="[('type','=','other'),('user_type.code','=','income')]"),
-                'account_revenue_id': fields.many2one('account.account', u'Conta Receita Padrao',),
+                'account_expense_id': fields.many2one('account.account', u'Conta Despesa Padrao',domain="[('type','=','other'),('user_type.code','=','expense')]"),
+                'account_revenue_id': fields.many2one('account.account', u'Conta Receita Padrao',domain="[('type','=','other'),('user_type.code','=','income')]"),
                 }
 
 res_company()
